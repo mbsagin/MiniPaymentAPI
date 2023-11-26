121537abd5c0cfc70f348e09f16fa39f9c277404
mbsagin <msagin@smartpulse.io>
2023-11-26 14:47:59 +0300

    Services collection

---
+++ BankServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using MiniPaymentAPI.Services;
+using MiniPaymentAPI.Services.Contracts;
+
+namespace MiniPaymentAPI
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddBankServices(this IServiceCollection services)
+        {
+            services.AddScoped<IPaymentService, PaymentService>();
+            services.AddScoped<IBankService, AkbankService>();
+            services.AddScoped<IBankService, GarantiService>();
+            services.AddScoped<IBankService, YapiKrediService>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MiniPaymentAPI;
 using MiniPaymentAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<MiniPaymentAPIContext>(options =>
                 options.UseInMemoryDatabase("MiniPaymentAPI"));
 
+builder.Services.AddBankServices();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MiniPaymentApi", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -17,7 +25,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MiniPaymentApi V1");
+    });
 }
 
 app.UseHttpsRedirection();

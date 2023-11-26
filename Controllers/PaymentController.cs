f0cef7d2b30dd84b7bfc2da4b688ecaa8b5b5984
mbsagin <msagin@smartpulse.io>
2023-11-26 14:48:08 +0300

    Payment API

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniPaymentAPI.Services.Contracts;
+
+namespace MiniPaymentAPI.Controllers
+{
+    [ApiController]
+    [Route("api/payment")]
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost("pay")]
+        public async Task<IActionResult> Pay([FromBody] string bankId, [FromBody] decimal amount, [FromBody] string orderReference)
+        {
+            return Ok(await _paymentService.ProcessPayment(bankId, amount, orderReference));
+        }
+
+        [HttpPost("cancel")]
+        public async Task<IActionResult> Cancel([FromQuery] string transactionId)
+        {
+            return Ok(await _paymentService.ProcessCancellation(transactionId));
+        }
+
+        [HttpPost("refund")]
+        public async Task<IActionResult> Refund([FromQuery] string transactionId)
+        {
+            return Ok(await _paymentService.ProcessRefund(transactionId));
+        }
+    }
+}
